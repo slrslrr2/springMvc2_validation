9ebcab243ba0be8573aa2837679739fdf1ed0b3b
slrslrr2 <slrslrr1@gmail.com>
2022-04-16 18:53:13 +0900

    검증2 BeanValidation 	- 등록/수정을 Item이라는 객체 하나도 Validation 체크 및 등록한 경우

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,17 +12,19 @@ import javax.validation.constraints.NotNull;
 // 아래 script를 적용할 수 있지만 실무는 더 복잡하기에 기능이 약해서 Controller에서 직접 자바코드 오류를 작성하는것을 권장한다.
 //@ScriptAssert(lang = "javascript", script = "_this.price + _this.quantity >= 10000", message = "가격*수량은 10000원을 넘어야합니다.")
 public class Item {
+
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +18,9 @@ import java.util.List;
 
 @Slf4j
 @Controller
-@RequestMapping("/validation/v3/items")
+@RequestMapping("/validation/v4/items")
 @RequiredArgsConstructor
-public class ValidationItemControllerV3 {
+public class ValidationItemControllerV4 {
 
     private final ItemRepository itemRepository;
 
@@ -37,20 +34,20 @@ public class ValidationItemControllerV3 {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "validation/v3/items";
+        return "validation/v4/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v3/item";
+        return "validation/v4/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
-        return "validation/v3/addForm";
+        return "validation/v4/addForm";
     }
 
     // 사용자 압력한 값이 남도록
@@ -67,14 +64,14 @@ public class ValidationItemControllerV3 {
         // 검증에 실패하면 다시 입력폼으로 이동
         if(bindingResult.hasErrors()){
             log.info("bindingResult {} ", bindingResult);
-            return "validation/v3/addForm";
+            return "validation/v4/addForm";
         }
 
         // 성공로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4/items/{itemId}";
     }
 
 
@@ -91,21 +88,21 @@ public class ValidationItemControllerV3 {
         // 검증에 실패하면 다시 입력폼으로 이동
         if(bindingResult.hasErrors()){
             log.info("bindingResult {} ", bindingResult);
-            return "validation/v3/addForm";
+            return "validation/v4/addForm";
         }
 
         // 성공로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v3/editForm";
+        return "validation/v4/editForm";
     }
 
 //    @PostMapping("/{itemId}/edit")
@@ -121,11 +118,11 @@ public class ValidationItemControllerV3 {
         // 검증에 실패하면 다시 입력폼으로 이동
         if(bindingResult.hasErrors()){
             log.info("bindingResult {} ", bindingResult);
-            return "validation/v3/editForm";
+            return "validation/v4/editForm";
         }
 
         itemRepository.update(itemId, item);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4/items/{itemId}";
     }
 
     @PostMapping("/{itemId}/edit")
@@ -141,11 +138,11 @@ public class ValidationItemControllerV3 {
         // 검증에 실패하면 다시 입력폼으로 이동
         if(bindingResult.hasErrors()){
             log.info("bindingResult {} ", bindingResult);
-            return "validation/v3/editForm";
+            return "validation/v4/editForm";
         }
 
         itemRepository.update(itemId, item);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4/items/{itemId}";
     }
 
 }

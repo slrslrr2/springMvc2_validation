41770e60a617b3492f3e99e029b2267c6a011afc
slrslrr2 <slrslrr1@gmail.com>
2022-04-22 22:50:17 +0900

    @Validated Form 등록/수정 분리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,30 +1,24 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 // 아래 script를 적용할 수 있지만 실무는 더 복잡하기에 기능이 약해서 Controller에서 직접 자바코드 오류를 작성하는것을 권장한다.
 //@ScriptAssert(lang = "javascript", script = "_this.price + _this.quantity >= 10000", message = "가격*수량은 10000원을 넘어야합니다.")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV4.java
@@ -4,6 +4,8 @@ import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.SaveCheck;
 import hello.itemservice.domain.item.UpdateCheck;
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import hello.itemservice.web.validation.form.ItemUpdateForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -46,40 +48,16 @@ public class ValidationItemControllerV4 {
 
     @GetMapping("/add")
     public String addForm(Model model) {
-        model.addAttribute("item", new Item());
+        model.addAttribute("itemSaveForm", new ItemSaveForm());
         return "validation/v4/addForm";
     }
 
-    // 사용자 압력한 값이 남도록
-//    @PostMapping("/add")
-    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-        // 특정 필드가 아닌 복합 룰은 자바코드로 직접 적용하는것을 권장
-        if(item.getQuantity() != null && item.getPrice() != null) {
-            int resultPrice = item.getQuantity() * item.getPrice();
-            if(resultPrice < 10000){
-                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
-            }
-        }
-
-        // 검증에 실패하면 다시 입력폼으로 이동
-        if(bindingResult.hasErrors()){
-            log.info("bindingResult {} ", bindingResult);
-            return "validation/v4/addForm";
-        }
-
-        // 성공로직
-        Item savedItem = itemRepository.save(item);
-        redirectAttributes.addAttribute("itemId", savedItem.getId());
-        redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v4/items/{itemId}";
-    }
-
-
     @PostMapping("/add")
-    public String addItem2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItem(@Validated @ModelAttribute ItemSaveForm form
+            , BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // 특정 필드가 아닌 복합 룰은 자바코드로 직접 적용하는것을 권장
-        if(item.getQuantity() != null && item.getPrice() != null) {
-            int resultPrice = item.getQuantity() * item.getPrice();
+        if(form.getQuantity() != null && form.getPrice() != null) {
+            int resultPrice = form.getQuantity() * form.getPrice();
             if(resultPrice < 10000){
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
@@ -92,7 +70,13 @@ public class ValidationItemControllerV4 {
         }
 
         // 성공로직
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
+
         Item savedItem = itemRepository.save(item);
+
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v4/items/{itemId}";
@@ -101,35 +85,15 @@ public class ValidationItemControllerV4 {
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
+        model.addAttribute("itemUpdateForm", item);
         return "validation/v4/editForm";
     }
 
-//    @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
-        // 특정 필드가 아닌 복합 룰은 자바코드로 직접 적용하는것을 권장
-        if(item.getQuantity() != null && item.getPrice() != null) {
-            int resultPrice = item.getQuantity() * item.getPrice();
-            if(resultPrice < 10000){
-                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
-            }
-        }
-
-        // 검증에 실패하면 다시 입력폼으로 이동
-        if(bindingResult.hasErrors()){
-            log.info("bindingResult {} ", bindingResult);
-            return "validation/v4/editForm";
-        }
-
-        itemRepository.update(itemId, item);
-        return "redirect:/validation/v4/items/{itemId}";
-    }
-
     @PostMapping("/{itemId}/edit")
-    public String edit2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute ItemUpdateForm form, BindingResult bindingResult) {
         // 특정 필드가 아닌 복합 룰은 자바코드로 직접 적용하는것을 권장
-        if(item.getQuantity() != null && item.getPrice() != null) {
-            int resultPrice = item.getQuantity() * item.getPrice();
+        if(form.getQuantity() != null && form.getPrice() != null) {
+            int resultPrice = form.getQuantity() * form.getPrice();
             if(resultPrice < 10000){
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
@@ -141,6 +105,11 @@ public class ValidationItemControllerV4 {
             return "validation/v4/editForm";
         }
 
+        Item item = new Item();
+        item.setId(form.getId());
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
         itemRepository.update(itemId, item);
         return "redirect:/validation/v4/items/{itemId}";
     }
